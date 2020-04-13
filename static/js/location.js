4ab5a5cc4c0be338f180f61b78e53771c083f25b
Daman Bhola <42509298+damnbhola@users.noreply.github.com>
2020-04-13 18:57:04 +0530

    Create location.js

---
+++ static/js/location.js
@@ -0,0 +1,18 @@
+let user_lat = undefined;
+let user_long = undefined;
+
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  user_lat = position.coords.latitude;
+  user_long = position.coords.longitude;
+  t = user_lat + "/" + user_long;
+  p.html(t)
+}
