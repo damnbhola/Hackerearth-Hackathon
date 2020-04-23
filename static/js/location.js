8be38ba4509c044aab120436ecee885a3ebbc958
Daman Bhola <42509298+damnbhola@users.noreply.github.com>
2020-04-24 03:43:58 +0530

    Update location.js

--- static/js/location.js
+++ static/js/location.js
@@ -14,12 +14,5 @@ function showPosition(position) {
   user_lat = position.coords.latitude;
   user_long = position.coords.longitude;
   t = "https://apiforcorona.herokuapp.com/location/" + user_lat + "/" + user_long;
-
-  var request = new XMLHttpRequest()
-  request.open('GET', t, true)
-  request.onload = function() {
-    var data = JSON.parse(this.response)
-    p.html(data);
-  }
-  request.send()
+  p.html(t);
 }
